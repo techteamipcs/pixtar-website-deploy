da5df2082b587dfe95d355ef16c211306cb35d7c
Muzaffar <muzaffar.ipcs@gmail.com>
2026-08-03 15:42:45 +0530

    new dep

--- pixtar-website/server/angular-app-manifest.mjs
+++ pixtar-website/server/angular-app-manifest.mjs
@@ -8,7 +8,7 @@ export default {
   {
     "renderMode": 0,
     "preload": [
-      "chunk-2XBWONL2.js",
+      "chunk-ETHH5VH6.js",
       "chunk-B34EPDR6.js",
       "chunk-FWD74DAW.js"
     ],
@@ -17,7 +17,7 @@ export default {
   {
     "renderMode": 0,
     "preload": [
-      "chunk-2XBWONL2.js",
+      "chunk-ETHH5VH6.js",
       "chunk-B34EPDR6.js",
       "chunk-FWD74DAW.js"
     ],
@@ -538,8 +538,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 17238, hash: '4eb72fa7494b8383d2c2f083571e0e5d4369448ab3bf867fcc99a7f43f8b15da', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 15062, hash: 'b941c66bd19fea34f02b7b5cb670d86f60a323df457ba871683ff652a89c089b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 17238, hash: '396325eab5c1d3dd995ebfb5c06822b6bc8c1dd6167d35cd7eb463b45c2e932c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 15062, hash: 'a0b2516af642c32c63c7cb7a693852713b83c4b37431ec36525fde9e73b047dc', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-QGXWO36J.css': {size: 14020, hash: 'VXRf7DHWMJs', text: () => import('./assets-chunks/styles-QGXWO36J_css.mjs').then(m => m.default)}
   },
 };
